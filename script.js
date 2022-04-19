4a85e0a7195849ce821a533d5d783377e7e00c88
jasmeen14 <jasmeen6199@gmail.com>
2022-04-19 14:47:09 +0200

    changes gif opacity in js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let songIndex = 0;
 let audioElement = new Audio("music/ranjha.mp3");
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
 
 let songs = [
     {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
@@ -28,15 +29,17 @@ masterPlay.addEventListener('click',()=>{
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
    }
    else{
     audioElement.pause();
     masterPlay.classList.remove('fa-circle-pause');
-    masterPlay.classList.add('fa-circle-play');   
+    masterPlay.classList.add('fa-circle-play'); 
+    gif.style.opacity = 0;  
    }
 })
 //listen events
-myProgressBar.addEventListener('timeupdate',()=>{
+audioElement.addEventListener('timeupdate',()=>{
      console.log('timeupdate');
      //Update Seekbar
 })
