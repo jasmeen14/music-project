4d6840e968514d0a54cc74244bf41d1c54abf91f
jasmeen14 <jasmeen6199@gmail.com>
2022-04-19 14:15:53 +0200

    started with javascript added the songs list

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Welcome");
+let songs = [
+    {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
+    {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
+    {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
+    {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
+    {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
+    {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
+    {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
+    {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
+    {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
+    {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
+]
+let audioElement = new Audio("music/ranjha.mp3");
+//audioElement.play();
