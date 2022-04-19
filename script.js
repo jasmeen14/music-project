f2b5e0b1d49345f3ef97f369cea126c40eaad7ab
jasmeen14 <jasmeen6199@gmail.com>
2022-04-19 14:38:05 +0200

    added play and pause button working

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 console.log("Welcome");
+
+//initialize the variables
+let songIndex = 0;
+let audioElement = new Audio("music/ranjha.mp3");
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+
 let songs = [
     {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
     {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
@@ -11,5 +18,25 @@ let songs = [
     {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
     {songName: "Ranjha",filePath: "music/ranjha.mp3",coverPath: "images/ranjha.jpeg"},
 ]
-let audioElement = new Audio("music/ranjha.mp3");
+ 
+
 //audioElement.play();
+
+//Handle Play Pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+   }
+   else{
+    audioElement.pause();
+    masterPlay.classList.remove('fa-circle-pause');
+    masterPlay.classList.add('fa-circle-play');   
+   }
+})
+//listen events
+myProgressBar.addEventListener('timeupdate',()=>{
+     console.log('timeupdate');
+     //Update Seekbar
+})
